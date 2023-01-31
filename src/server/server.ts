0f7ad82c3e029c885f42c821e711412b9d590e44
Greg Jenner <jennergreg733@gmail.com>
2023-01-31 12:34:18 -0500

    update server to route to oauth

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,6 +4,10 @@ import express, {
   NextFunction,
   RequestHandler,
 } from 'express';
+import oauthController from './controllers/oauthController';
+import cookieController from './controllers/cookieController';
+import userController from './controllers/userController';
+import sessionController from './controllers/sessionController';
 
 type ServerError = string;
 
@@ -12,14 +16,21 @@ const app = express();
 app.use(express.json());
 
 // API that redirects to GitHub to authorize the account
+// Successful attempts will redirect to http://localhost:8080/oauth2callback with parameter "code" in query string
 
 const clientId = 'fc863cedc9ecd45da5de';
 const clientSecret = 'da6f0104ab5cf5d90cbd5cb6abf46b69e09ac10a';
 
-app.get('/', (req, res) => {
+app.get('/oauth', (req, res) => {
   res.redirect(
     `https://github.com/login/oauth/authorize?client_id=${clientId}`
   );
+});
+
+//get call to /oauth2callback comes directly from GitHub after a successful login
+
+app.get('/oauth2callback', oauthController.setToken, (req, res) => {
+  res.response(200).redirect('./Homepage.tsx');
 });
 
 // user controller, cookie controller, session controller
