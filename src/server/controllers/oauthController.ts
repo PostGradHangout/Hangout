8e4c1d5b06942f9186d581100dd84d188171fd5f
Greg Jenner <jennergreg733@gmail.com>
2023-01-31 20:49:44 -0500

    retrieve access token and user information in setToken

--- src/server/controllers/oauthController.ts
+++ src/server/controllers/oauthController.ts
@@ -1,53 +1,76 @@
 // @ts-nocheck
 
+import { response } from 'express';
 import { Pool } from 'pg';
 // const db = require('../models/userModel.ts');
-import {db} from '../models/sqlModel';
-
-
-
+import { db } from '../models/sqlModel';
 
 export const oauthController = {
+  //=============================
+  // oAuth Set Token
+  //=============================
 
-//=============================
-// oAuth Set Token
-//=============================
-
-  setToken:  (req, res, next) => {
+  setToken: (req, res, next) => {
     console.log('storeToken fired');
     console.log('req.query.code: ', req.query.code);
     const clientId = 'fc863cedc9ecd45da5de';
     const clientSecret = 'da6f0104ab5cf5d90cbd5cb6abf46b69e09ac10a';
-      const body = {client_id: clientId, client_secret: clientSecret, code: req.query.code}
-  fetch(`https://github.com/login/oauth/access_token`, {
+    const body = {
+      client_id: clientId,
+      client_secret: clientSecret,
+      code: req.query.code,
+    };
+    fetch(`https://github.com/login/oauth/access_token`, {
       method: 'POST',
       headers: {
-          'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
       },
-      body: JSON.stringify(body)
-  })
+      body: JSON.stringify(body),
+    })
+      .then((response) => response.json())
       .then((response) => {
         console.log('successfully created user');
-        console.log('response[access_token]: ', response[access_token]);
-        return next();
+        console.log('response: ', response);
+        fetch('https://api.github.com/user', {
+          method: 'GET',
+          headers: {
+            Authorization: `${response.token_type} ${response.access_token}`,
+            Accept: 'application/json',
+          },
+        })
+          .then((response) => response.json())
+          .then((response) => {
+            console.log('response from github api', response);
+            res.locals.username = response.login;
+            return next();
+          })
+          .catch((err) => {
+            next({
+              log: `Error in oauthController.setToken in request to github api. Details: ${err}`,
+              message: {
+                err: 'An error occurred in the oauthController.setToken',
+              },
+            });
+          });
       })
-     .catch((err) => {
-      next({
-        log: `Error in oauthController.setToken. Details: ${err}`,
-        message: { err: 'An error occurred in the oauthController.setToken' },
+      .catch((err) => {
+        next({
+          log: `Error in oauthController.setToken. Details: ${err}`,
+          message: { err: 'An error occurred in the oauthController.setToken' },
+        });
       });
-    })
-    
   },
   redirect: (req, res, next) => {
     console.log('redirect fired');
     return next();
-    if (err) {((err) => {
-      return next({
-        log: `Error in oauthController.setToken. Details: ${err}`,
-        message: { err: 'An error occurred in the oauthController.setToken' },
-      });
-    })
-  }
+    if (err) {
+      (err) => {
+        return next({
+          log: `Error in oauthController.setToken. Details: ${err}`,
+          message: { err: 'An error occurred in the oauthController.setToken' },
+        });
+      };
+    }
   },
 };
